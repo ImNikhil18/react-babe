c0b89439bfa290ec6cdd986a7f4b57fb0b0dace3
ImNikhil18 <nikhilparlapalli@users.noreply.github.com>
2024-01-28 12:10:28 +0530

    new files added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(navbar)
+
+// ReactDOM.render(navbar, document.getElementById("root"))
+// ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
+
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
+
+// take navbar
+
+// document.getElementById("root").append(JSON.stringify(navbar))
+
+/**
+Challenge: fix our code!
+
+Don't forget, you're not using CDNs anymore, so there's no
+global "ReactDOM" variable to use anymore.
+ */
+
+
+const page = (
+  <div>
+      <h1>My awesome website in React</h1>
+      <h3>Reasons I love React</h3>
+      <ol>
+          <li>It's composable</li>
+          <li>It's declarative</li>
+          <li>It's a hireable skill</li>
+          <li>It's actively maintained by skilled people</li>
+      </ol>
+  </div>
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(page)
+// document.getElementById("root").append(JSON.stringify(page))
